b8665029f1e600ea59643049d2bb3a12605e7016
Elena Kechina <elenakechina86@gmail.com>
2021-08-31 23:35:46 +0300

    HomeWork#2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,47 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min, max, sum, avg;
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
 
-  // Ваш код
+  arr.forEach((element) => {
+    if (min > element) {
+      min = element;
+    }
 
-  return { min:min, max:max, avg:avg };
+    if (max < element) {
+      max = element;
+    }
+    sum += element;
+  });
+  avg = +(sum / arr.length).toFixed(2);
+  return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
   let sum;
-
-  // Ваш код
+  sum = arr.reduce((acc, elem) => acc + elem, 0);
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max;
+  max = func(arrOfArr[0]);
 
-  // Ваш кода
-  // for ...
-  
-  return max
+  arrOfArr.forEach((arr) => {
+    let funcResult = func(arr);
+    if (max < funcResult) {
+      max = funcResult;
+    }
+  });
+
+  return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  return Math.max(...arr) - Math.min(...arr);
 }
