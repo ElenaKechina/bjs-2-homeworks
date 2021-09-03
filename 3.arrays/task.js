549ecebabac8d23bb0414a9c8a963918afbd5f20
Elena Kechina <elenakechina86@gmail.com>
2021-09-03 23:52:46 +0300

    homeWork_3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  result = arr1.every((value, index) => value === arr2[index]);
 
   return result; // boolean
 }
@@ -9,7 +13,9 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr
+    .filter((value) => value % 3 === 0 && value >= 0)
+    .map((value) => value * 10);
 
   return resultArr; // array
 }
