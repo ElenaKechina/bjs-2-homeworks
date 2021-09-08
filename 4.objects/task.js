ef59b2206588c29391e5c0a0f7e21a1ff6dd4735
Elena Kechina <elenakechina86@gmail.com>
2021-09-09 00:09:20 +0300

    homeWork_4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks) {
+    this.marks.push(mark);
+  } else {
+    this.marks = [mark];
+  }
+};
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks) {
+    this.marks.push(...marks);
+  } else {
+    this.marks = [...marks];
+  }
+};
+
+Student.prototype.getAverage = function () {
+  if (!this.marks) {
+    return 0;
+  }
+  const sum = this.marks.reduce((acc, element) => acc + element, 0);
+  return sum / this.marks.length;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+};
